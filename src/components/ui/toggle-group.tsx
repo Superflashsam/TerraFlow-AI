58a6d35ef64e8f7be386ec7971b43e688bd04e62
TerraFlow05 <terraflow78@gmail.com>
2025-10-23 16:35:56 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/components/ui/toggle-group.tsx
+++ src/components/ui/toggle-group.tsx
@@ -37,7 +37,7 @@ const ToggleGroupContext = React.createContext<
 
 const ToggleGroup = React.forwardRef<
   React.ElementRef<typeof ToggleGroupPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Root> &amp;
+  React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Root> &
     VariantProps<typeof toggleGroupVariants>
 >(({ className, variant, size, children, ...props }, ref) => (
   <ToggleGroupPrimitive.Root
@@ -53,7 +53,7 @@ const ToggleGroup = React.forwardRef<
 
 const ToggleGroupItem = React.forwardRef<
   React.ElementRef<typeof ToggleGroupPrimitive.Item>,
-  React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Item> &amp;
+  React.ComponentPropsWithoutRef<typeof ToggleGroupPrimitive.Item> &
     VariantProps<typeof toggleGroupVariants>
 >(({ className, children, variant, size, ...props }, ref) => {
   const context = React.useContext(ToggleGroupContext)
