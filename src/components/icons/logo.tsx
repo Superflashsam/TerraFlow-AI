4133f87fe109769f20c602423c5505ac30f17e97
TerraFlow05 <terraflow78@gmail.com>
2025-10-24 21:37:21 +0000

    Try fixing this error: `Runtime Error: Error: Cannot read properties of

--- src/components/icons/logo.tsx
+++ src/components/icons/logo.tsx
@@ -1,7 +1,7 @@
-import type { SVGProps } from "react";
 import Image from "next/image";
+import type { ComponentProps } from "react";
 
-export function Logo(props: SVGProps<SVGSVGElement>) {
+export function Logo(props: Partial<ComponentProps<typeof Image>>) {
   return (
     <Image 
       src="https://res.cloudinary.com/dvic0tda9/image/upload/e_improve,e_sharpen/v1761341547/generated-image_1_k77rij_e_improve_e_sharpen-removebg-preview_lowavj.png"
